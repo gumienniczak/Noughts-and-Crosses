40513609731f52152c9d79c70dea670d104c7114
gumienniczak <adam.gumieniczek@gmail.com>
2025-03-05 16:40:29 +0000

    Added more functions so that the computer can make guessess too

--- tictactoe.py
+++ tictactoe.py
@@ -4,46 +4,61 @@ board = [[1, 2, 3],
          [4, 5, 6],
          [7, 8, 9]]
 
-board_art = {'empty': ("┌─────────┐",
-                       "│         │",
-                       "│         │",
-                       "│         │",
-                       "└─────────┘"),
-             'x': ("┌─────────┐",
-                   "│   \ /   │",
-                   "│    X    │",
-                   "│   / \   │",
-                   "└─────────┘"),
-             'o': ("┌─────────┐",
-                   "│    ︵   │",
-                   "│  (   )  │",
-                   "│    ︶   │",
-                   "└─────────┘")}
+possible_moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+used_moves = []
+
+# board_art = {'empty': ("┌─────────┐",
+#                        "│         │",
+#                        "│         │",
+#                        "│         │",
+#                        "└─────────┘"),
+#              'x': ("┌─────────┐",
+#                    "│   \ /   │",
+#                    "│    X    │",
+#                    "│   / \   │",
+#                    "└─────────┘"),
+#              'o': ("┌─────────┐",
+#                    "│    ︵   │",
+#                    "│  (   )  │",
+#                    "│    ︶   │",
+#                    "└─────────┘")}
+
+board_art = {'empty': "[ ]",
+             'x': "[X]",
+             'o': '[O]'}
 
 
 def print_board(board):
     for row in board:
-        for value in board:
-            if value.lower() == 'x':
+        for value in row:
+            if str(value).lower() == 'x':
                 print(board_art['x'], end=' ')
-            elif value.lower() == 'o':
+            elif str(value).lower() == 'o':
                 print(board_art['o'], end=' ')
             else:
                 print(board_art['empty'], end=' ')
         print()
 
 
+number_to_position_map = {1: (0, 0), 2: (0, 1), 3: (0, 2),
+                          4: (1, 0), 5: (1, 1), 6: (1, 2),
+                          7: (2, 0), 8: (2, 1), 9: (2, 2)}
+
+
 def ask_for_input():
     while True:
         user_choice = input('Choose field you would like to mark: ')
-        if int(user_choice) in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
-            number_to_position_map = {1: (0, 0), 2: (0, 1), 3: (0, 2),
-                                      4: (1, 0), 5: (1, 1), 6: (1, 2),
-                                      7: (2, 0), 8: (2, 1), 9: (2, 2)}
-            hor_i = number_to_position_map[user_choice][0]
-            ver_i = number_to_position_map[user_choice][1]
+        chosen_number = int(user_choice)
+        if chosen_number in possible_moves and chosen_number not in used_moves:
+            possible_moves.remove(int(user_choice))
+            used_moves.append(chosen_number)
+            hor_i = number_to_position_map[chosen_number][0]
+            ver_i = number_to_position_map[chosen_number][1]
 
             return hor_i, ver_i
+        elif chosen_number in used_moves:
+            print('This field has already been marked!')
+            break
 
         else:
             print('The input is invalid')
@@ -74,8 +89,28 @@ def check_if_winning(board):
         winning = True
     elif board[0][2] == 'x' and board[1][1] == 'x' and board[2][0] == 'x':
         winning = True
+    return winning
 
 
-print(board)
-x = update_board(1, 1)
-print(x)
+def computer_marks():
+    computer_move = random.choice(possible_moves)
+    possible_moves.remove(computer_move)
+    used_moves.append(computer_move)
+    hor_i = number_to_position_map[computer_move][0]
+    ver_i = number_to_position_map[computer_move][1]
+    return hor_i, ver_i
+
+
+def main():
+    global board
+    while possible_moves and check_if_winning(board) is False:
+        move = ask_for_input()
+        board = update_board(move[0], move[1])
+        print_board(board)
+        computer_move = ask_for_input()
+        board = update_board(computer_move[0], computer_move[1])
+        print_board(board)
+
+
+if __name__ == '__main__':
+    main()
